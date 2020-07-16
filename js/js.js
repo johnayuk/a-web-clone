7390382a903f5811726d238f40a873d789e11610
johnayuk <john1234ayk@gmail.com>
2020-07-17 00:21:54 +0100

    project

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ AOS.init();
 
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "450px";
+    document.getElementById("mySidenav").style.width = "399px";
 }
 
 function closeNav(a) {
