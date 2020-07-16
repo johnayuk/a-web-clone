8038cf9272c194bca02fb5e84c523962cda4ebd5
johnayuk <john1234ayk@gmail.com>
2020-07-16 10:21:17 +0100

    clone

---
+++ js/js.js
@@ -0,0 +1,11 @@
+AOS.init(); 
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+}
+
+function closeNav(a) {
+a.preventDefault
+    document.getElementById("mySidenav").style.width = "0";
+}
